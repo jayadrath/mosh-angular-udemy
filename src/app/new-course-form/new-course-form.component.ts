e22baa91ff739570e79e0e543cdfecb8cc50e05e
m62803 <kacha@nuuday.dk>
2020-02-20 16:10:44 +0530

    Reactive-forms final

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -7,17 +7,22 @@ import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@ang
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent{
-  form;
+  form = new FormGroup({
+    topics : new FormArray([])
+  });
 
-  constructor(fb: FormBuilder){
-    this.form = fb.group({
-      name: ['', Validators.required],
-      contact: fb.group({
-        email: [],
-        phone: []
-      }),
-      topics: fb.array([])
-    });
+  addTopic(topic: HTMLInputElement){
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  removeTopic(topic: FormControl){
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+  get topics(){
+    return this.form.get('topics') as FormArray;
   }
   
 }

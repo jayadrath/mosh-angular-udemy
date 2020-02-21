f1a1e45f7e1df0033fecdb87d2c92ec12e52d535
m62803 <kacha@nuuday.dk>
2020-02-21 17:56:17 +0530

    Consuming http services - Post data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,14 +8,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PostsComponent{
   posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts'; 
 
-  constructor(http: HttpClient) { 
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient) { 
+    http.get(this.url)
         .subscribe(response =>{
           this.posts = response as any[];
         })
   }
 
+  createPost(input:HTMLInputElement){
+    let post = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+        .subscribe(response =>{
+          post['id'] = response['id'];
+          this.posts.splice(0,0,post);
+          console.log(response);
+        });
+  }
  
 
 }

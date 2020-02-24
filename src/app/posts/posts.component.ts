fd16d19b931a39b5ba3ba441a03e2bd55364dd5e
m62803 <kacha@nuuday.dk>
2020-02-24 13:09:45 +0530

    Consuming http services - Deleting data from jsonplaceholder

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -34,6 +34,14 @@ export class PostsComponent{
         console.log(response);
       })
   }
+
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id)
+    .subscribe(response => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index,1);
+    });
+  }
  
 
 }

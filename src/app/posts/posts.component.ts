9501ba30a9173cdacd0114c5ff8bb7d98c7b56d3
m62803 <kacha@nuuday.dk>
2020-02-24 12:38:10 +0530

    Consuming http services - Updating data from jsonplaceholder

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -27,6 +27,13 @@ export class PostsComponent{
           console.log(response);
         });
   }
+
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+      .subscribe(response => {
+        console.log(response);
+      })
+  }
  
 
 }
